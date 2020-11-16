c0fbe8405749094e768712bb58ee5bdef1cf8095
dobschal <s.dobschal@tdsoftware.de>
2020-11-16 17:01:03 +0100

    yeah

--- src/components/uiElements/DailyReward.vue
+++ src/components/uiElements/DailyReward.vue
@@ -104,6 +104,12 @@
             text-align: center;
             padding: 0.05rem 0.2rem;
             transform: rotate(5deg);
+            -webkit-touch-callout: none; /* iOS Safari */
+            -webkit-user-select: none; /* Safari */
+            -khtml-user-select: none; /* Konqueror HTML */
+            -moz-user-select: none; /* Old versions of Firefox */
+            -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none;
         }
     }
 </style>
